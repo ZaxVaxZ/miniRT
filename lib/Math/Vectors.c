51f7ce8efec08cf412c41fab7ee44151e5579439
EmadHammoudeEJ <emad.hammoude@camb.ai>
2024-09-03 23:21:23 +0400

    Added some vector math

--- lib/Math/Vectors.c
+++ lib/Math/Vectors.c
@@ -21,7 +21,7 @@ void	cross(t_vector *result, t_vector v1, t_vector v2)
 {
 	result->x = v1.y * v2.z - v1.z * v2.y;
 	result->y = v1.z * v2.x - v1.x * v2.z;
-	result->z = v1.y * v2.x - v1.x * v2.y;
+	result->z = v1.x * v2.y - v1.y * v2.x;
 }
 
 float	magnitude(t_vector v)
