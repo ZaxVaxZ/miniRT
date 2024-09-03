d4f7d8607e735c036800ee080c5d7b888845824c
EmadHammoudeEJ <emad.hammoude@camb.ai>
2024-09-04 00:55:19 +0400

    added vector math

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include "ft_printf.h"
 
+void	init_main_struct(t_main *m)
+{
+	m->mlx = NULL;
+	m->mw = NULL;
+	m->img = NULL;
+	m->pixel = NULL;
+}
+
 void	color_pixel(t_main m, int x, int y, int color)
 {
 	int	pixel_index;
@@ -21,6 +29,38 @@ void	color_pixel(t_main m, int x, int y, int color)
 		m.pixel[pixel_index + 1] = (color >> 8) & 0xFF;
 		m.pixel[pixel_index + 2] = (color >> 16) & 0xFF;
 		m.pixel[pixel_index + 3] = (color >> 24);
+	}
+}
+
+void	draw_rainbow(t_main m)
+{
+	int	a, b, c, color;
+
+	for(int y = 0; y < HEIGHT; ++y)
+	{
+		a = 0;
+		b = 0;
+		c = 255;
+		for(int x = 0; x < WIDTH; ++x)
+		{
+			if (!a && c > 0)
+			{
+				b++;
+				c--;
+			}
+			if (b > 0 && !c)
+			{
+				b--;
+				a++;
+			}
+			if (!b && a > 0)
+			{
+				a--;
+				c++;
+			}
+			color = (a << 16) + (b << 8) + c;
+			color_pixel(m, x, y, color);
+		}
 	}
 }
 
@@ -65,41 +105,19 @@ int	main( void )
 {
 	t_main	m;
 
+	init_main_struct(&m);
 	m.mlx = mlx_init();
+	if (!m.mlx)
+		free_and_exit(&m, FAILURE, 1);
 	m.mw = mlx_new_window(m.mlx, WIDTH, HEIGHT, "miniFarty");
 	m.img = mlx_new_image(m.mlx, WIDTH, HEIGHT);
+	if (!m.img)
+		free_and_exit(&m, FAILURE, 1);
 	m.pixel = mlx_get_data_addr(m.img, &m.pixel_bits, &m.line_bytes, &m.endian);
-	ft_printf("Hello!\n");
-	int a,b,c;
-	for(int y = 0; y < HEIGHT; ++y)
-	{
-		a = 0;
-		b = 0;
-		c = 255;
-		for(int x = 0; x < WIDTH; ++x)
-		{
-			if (!a && c > 0)
-			{
-				b++;
-				c--;
-			}
-			if (b > 0 && !c)
-			{
-				b--;
-				a++;
-			}
-			if (!b && a > 0)
-			{
-				a--;
-				c++;
-			}
-			int color;
-			color = (a << 16) + (b << 8) + c;
-			color_pixel(m, x, y, color);
-		}
-	}
-	if (m.img)
-		mlx_put_image_to_window(m.mlx, m.mw, m.img, 0, 0);
+	if (!m.mw || !m.pixel)
+		free_and_exit(&m, FAILURE, 1);
+	draw_rainbow(m);
+	mlx_put_image_to_window(m.mlx, m.mw, m.img, 0, 0);
 	mlx_hook(m.mw, 2, 1L << 0, &keypress_hook, &m);
 	mlx_hook(m.mw, 17, 1L << 2, exitbutton_hook, &m);
 	mlx_loop(m.mlx);
